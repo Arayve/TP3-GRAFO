97346d7c6a60f3f66cf60cfcc106b171d4c19688
Arayve <alejandro150998@outlook.com>
2018-06-20 01:20:21 -0300

    Agregando pertence cuidad

--- grafo.py
+++ grafo.py
@@ -5,7 +5,7 @@ class Ciudad:
 		self.long = longitud
 	def __str__(self):
 		return "Ciudad {},Latitud {},Longitud {})".format(self.nombre,self.lat,self.long)
-
+"""Grafo dirigido y pesado"""
 class Grafo:
 	def __init__(self):
 		self.raiz={}#cambiale el nombre si queres
@@ -14,13 +14,18 @@ class Grafo:
 		if ciudad in self.raiz:
 			return #Quiero preguntar , al profesor si debo levantar error
 		self.raiz[ciudad]={}
-	def agregar_arista(self,cuidad_partida,ciudad_llegada,tiempo):
+	def agregar_arista(self,cuidad_partida,ciudad_llegada,tiempo):#consultar que debo hacer si no existe cuidad de partida o llegada
 		"""Si no existe cuidad partida o llegada lo crea"""
-		aux=Campo(vertice_entrante,valor)
 		dic_ciudad=self.raiz.get(cuidad_partida,{})
 		dic_ciudad[ciudad_llegada]=tiempo
+	def pertenece_vertice(self,cuidad):
+		if cuidad in self.raiz:
+			return True
+	def peso_arista(self,cuidad_partida,ciudad_llegada):
+
 	def __str__(self):
 		return "{}".format(self.raiz)
+
 cuidad1 = Ciudad("Moscu",55.755833,37.617778)
 cuidad2 = Ciudad("San Petesburgo",59.950000,30.316667)
 cuidad3 = Ciudad("Kaliningrado",54.716667,20.500000)
@@ -34,5 +39,13 @@ grafo.agregar_vertice(cuidad3)
 grafo.agregar_vertice(cuidad4)
 grafo.agregar_vertice(cuidad5)
 
-for x in grafo.raiz.keys():
-	print(x)
+grafo.agregar_arista(cuidad1,cuidad2,34)
+if grafo.pertenece_vertice(cuidad1):
+	print("SI pertenece")
+"""
+for clave1,valor1 in grafo.raiz.items():
+	print(clave1)
+	for clave2,valor2 in valor1.items():
+		print(clave2)
+		print(valor2)
+"""
