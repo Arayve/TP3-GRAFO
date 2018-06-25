8e36cb29db8fd20d6e2a53ec52d8ac4d73e453f8
KethTatsu <ben.inneo@gmail.com>
2018-06-25 19:38:46 -0300

    Agregado sedes.csv

--- comandos.py
+++ comandos.py
@@ -67,20 +67,37 @@ def comparar_comando(grafo, linea_comando):
 
 	return False
 
-def cargar_set_datos(nombre_archivos):
+def cargar_set_datos(nombre_archivo_ciudades):
 
-	#PROCESO ARCHIVOS Y DEVUELVO EL GRAFO
+	grafo = Grafo(False)
+	coordenadas = {}
 
-	return None
+	archivo_ciudades = open(nombre_archivo_ciudades, "r")
+
+	linea_actual = archivo_ciudades.read() #El archivo sedes.csv tiene un "11" al principio y un "55" en el medio
+#NO LEE CORRECTAMENTE, DEBERIA LEER LINEA POR LINEA
+	while linea_actual != "55":
+		linea_actual = archivo_ciudades.read()
+		linea_ciudad = linea_actual.split(",")
+		grafo.agregar_vertice(linea_ciudad[0])
+		coordenadas[linea_ciudad[0]] = linea_ciudad
+
+	while linea_actual != "":
+		linea_actual = archivo_ciudades.read()
+		linea_ciudad = linea_actual.split(",")
+		grafo.agregar_arista(linea_ciudad[0], linea_ciudad[1], linea_ciudad[2])
+
+	archivo_ciudades.close()
+
+	return grafo, coordenadas
 
 def main():
-	if len(sys.argv) < 2:
+
+	if len(sys.argv) != 3:
 		print("Cantidad de parametros invalida")
 		return False
 
-	nombre_archivos = sys.argv
-
-	grafo = cargar_set_datos(nombre_archivos)
+	grafo, coordenadas = cargar_set_datos(sys.argv[1])
 
 	while True:
 		linea_actual = input()
@@ -88,7 +105,7 @@ def main():
 			return False
 
 		linea_comando = linea_actual.split(" ")
-		if not comparar_comando(grafo, linea_comando):
+		if not comparar_comando(grafo, linea_comando, sys.argv[2]):
 			print("Parametro incorrecto")
 			return False
 
